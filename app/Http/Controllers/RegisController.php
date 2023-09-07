e6b6a023c40615fb66a66365d70b612c6849835b
Maulida Nandar Saputra <maulidanandar97@gmail.com>
2023-09-07 20:32:45 +0700

    login . Register

---
+++ app/Http/Controllers/RegisController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisController extends Controller
+{
+    public function index()
+    {
+        return view('auth.regis');
+    }
+    public function store(Request $request)
+    {
+        // dd($request);
+        $validated = $request->validate([
+            // 'user_name' => 'min:6',
+            'name' => 'required|min:3|max:255',
+            'username' => 'required|unique:users',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'min:6|required_with:password_confirmation|same:password_confirmation',
+            'password_confirmation' => 'min:6'
+        // dd($request);
+
+        ]);
+        // dd($request);
+
+        $validated['password'] = Hash::make($validated['password']);
+    
+        User::create($validated);
+        return redirect('login')->with('success', 'Registration Successful, Please Login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisController;
+use App\Http\Controllers\DashboardController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/dashboard', function () {
+//     return view('layouts.app');
+// });
+
+
+Route::get('/register', [RegisController::class, 'index'])->name('register');
+Route::post('/register', [RegisController::class, 'store'])->name('register.create');;
+
+
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+
+
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
