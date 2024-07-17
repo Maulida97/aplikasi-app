971ba20a7c1c608c767c87612b5af6774e81a28d
Maulida Nandar Saputra <maulidanandar97@gmail.com>
2024-07-17 16:42:49 +0700

    update sensorcontroller

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -12,6 +12,8 @@ use App\Models\Sensorinfus;
 use Illuminate\Http\Request;
 use App\Services\SensorService;
 use Illuminate\Support\Facades\DB;
+use Spatie\FlareClient\Http\Client;
+use Illuminate\Support\Facades\Http;
 
 class SensorController extends Controller
 {
@@ -26,18 +28,27 @@ class SensorController extends Controller
     }
 
 
-    public function simpan($nilaiTetesan, $nilaiBerat)
+    public function simpan($nilaiTetesan, $nilaiBerat,$waktujam,$waktumenit)
     {
         $tgl = Carbon::now();
             $data = sensor::where('id', '1')->update([
                 "volume_infus" => $nilaiBerat,
                 "tetesan_infus" => $nilaiTetesan,
+                "waktu_jam" => $waktujam,
+                "waktu_menit" => $waktumenit,
                 "created_at" => $tgl,
                 "updated_at" => $tgl,
             ]);
 
+            // $data1 = sensor::where("id",'1')->first();
+            // $berat = $data1->volume_infus;
+            // $tetesan = $data1->tetesan_infus;
+            // $jam = $data1->waktu_jam;
+            // $menit = $data1->waktu_menit;
+            // $response = Http::get('https://edgeinfus.my.id/api/simpan/'.$tetesan.'/'.$berat.'/'.$jam.'/'.$menit);
+
     }
-    public function simpan2($nilaiTetesan2, $nilaiBerat2)
+    public function simpan2($nilaiTetesan2, $nilaiBerat2, $waktujam2, $waktumenit2)
     {
         // $data = DB::table('sensors');
         $tgl = Carbon::now();
@@ -45,13 +56,22 @@ class SensorController extends Controller
             $data = sensor::where('id', '2')->update([
                 "volume_infus" => $nilaiBerat2,
                 "tetesan_infus" => $nilaiTetesan2,
+                "waktu_jam" => $waktujam2,
+                "waktu_menit" => $waktumenit2,
                 "created_at" => $tgl,
                 "updated_at" => $tgl,
             ]);
 
+            // $data1 = sensor::where("id",'2')->first();
+            // $berat = $data1->volume_infus;
+            // $tetesan = $data1->tetesan_infus;
+            // $jam = $data1->waktu_jam;
+            // $menit = $data1->waktu_menit;
+            // $response = Http::get('https://edgeinfus.my.id/api/simpan2/'.$tetesan.'/'.$berat.'/'.$jam.'/'.$menit);
+
     }
 
-    public function simpan3($nilaiTetesan3, $nilaiBerat3)
+    public function simpan3($nilaiTetesan3, $nilaiBerat3, $waktujam3, $waktumenit3)
     {
         // $data = DB::table('sensors');
         $tgl = Carbon::now();
@@ -59,9 +79,19 @@ class SensorController extends Controller
             $data = sensor::where('id', '3')->update([
                 "volume_infus" => $nilaiBerat3,
                 "tetesan_infus" => $nilaiTetesan3,
+                "waktu_jam" => $waktujam3,
+                "waktu_menit" => $waktumenit3,
                 "created_at" => $tgl,
                 "updated_at" => $tgl,
             ]);
+
+            // $data1 = sensor::where("id",'3')->first();
+            // $berat = $data1->volume_infus;
+            // $tetesan = $data1->tetesan_infus;
+            // $jam = $data1->waktu_jam;
+            // $menit = $data1->waktu_menit;
+            // $response = Http::get('https://edgeinfus.my.id/api/simpan3/'.$tetesan.'/'.$berat.'/'.$jam.'/'.$menit);
+
 
     }
 
@@ -80,11 +110,14 @@ class SensorController extends Controller
         $sensor = sensor::where('id', '1')->get();
         return view('admin.dashboard.bacatetesan', ['nilaisensor'=> $sensor]);
     }
-    // public function bacatetesan(){
-    //     $sensor = Sensor::select('*')->get();
-    //     return view('admin.dashboard.bacatetesan', ['nilaisensor'=> $sensor]);
-    // }
-
+    public function bacajam(){
+        $sensor = sensor::where('id', '1')->get();
+        return view('admin.dashboard.bacajam', ['nilaisensor'=> $sensor]);
+    }
+    public function bacamenit(){
+        $sensor = sensor::where('id', '1')->get();
+        return view('admin.dashboard.bacamenit', ['nilaisensor'=> $sensor]);
+    }
         // Infus 2
     public function bacavolume2(){
         $sensor = sensor::where('id', '2')->get();
@@ -94,7 +127,14 @@ class SensorController extends Controller
         $sensor = sensor::where('id', '2')->get();
         return view('admin.dashboard.bacatetesan2', ['nilaisensor2'=> $sensor]);
     }
-
+    public function bacajam2(){
+        $sensor = sensor::where('id', '2')->get();
+        return view('admin.dashboard.bacajam2', ['nilaisensor2'=> $sensor]);
+    }
+    public function bacamenit2(){
+        $sensor = sensor::where('id', '2')->get();
+        return view('admin.dashboard.bacamenit2', ['nilaisensor2'=> $sensor]);
+    }
     // Infus 3
     public function bacavolume3(){
         $sensor = sensor::where('id', '3')->get();
@@ -104,6 +144,15 @@ class SensorController extends Controller
         $sensor = sensor::where('id', '3')->get();
         return view('admin.dashboard.bacatetesan3', ['nilaisensor3'=> $sensor]);
     }
+    public function bacajam3(){
+        $sensor = sensor::where('id', '3')->get();
+        return view('admin.dashboard.bacajam3', ['nilaisensor3'=> $sensor]);
+    }
+    public function bacamenit3(){
+        $sensor = sensor::where('id', '3')->get();
+        return view('admin.dashboard.bacamenit3', ['nilaisensor3'=> $sensor]);
+    }
+
     // public function simpan($nilaivolume, $nilaitetesan){
     //     Sensor::where('id', '1')->update([
     //         'berat_infus' => request()->nilaivolum,
